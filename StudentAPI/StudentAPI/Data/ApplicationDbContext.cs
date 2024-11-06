dcf699a4504f78795eab3646facb392ec52b227b
tayyeb <tayyebch@gmail.com>
2024-11-06 09:59:48 -0800

    Initial commit for StudentAPI project

---
+++ StudentAPI/StudentAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Models;
+
+namespace StudentAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) 
+        {
+        
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentAPI/StudentAPI/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentAPI.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; } = " ";
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+
+    }
+}
